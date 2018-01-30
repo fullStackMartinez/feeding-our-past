54aa386a3c81faf220d9b92232eafeac0858e760
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 11:59:41 -0700

    created persona-2.php with outline

---
+++ epic/persona-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Feeding Our Past: Persona 2</title>
+	</head>
+	<body>
+		<h1>Feeding Our Past: Persona 2</h1>
+
+		<!-- Define a Persona representing the target audience for the capstone project -->
+		<h2>Persona 2</h2>
+		<p><strong>Name</strong>: <br>
+			<strong>Age</strong>: </p>
+
+		<p><strong>Profession</strong>: </p>
+		<p><strong>Technology</strong>: </p>
+		<p><strong>Attitudes and Behaviors</strong>: </p>
+		<p><strong>Needs and Goals</strong>: </p>
+		<p><strong>User Story</strong>: </p>
+	</body>
+</html>
