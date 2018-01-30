4a8900fa540551a766b18781abf15bd3658e430a
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 15:06:03 -0700

    added comments at top and persona name/age

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -7,10 +7,10 @@
 	<body>
 		<h1>Feeding Our Past: Persona 2</h1>
 
-		<!-- Define a Persona representing the target audience for the capstone project -->
-		<h2>Persona 2</h2>
-		<p><strong>Name</strong>: <br>
-			<strong>Age</strong>: </p>
+		<!-- Define a Persona representing the target audience for the capstone project:
+		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor -->
+		<p><strong>Name</strong>: Diane Baker<br>
+			<strong>Age</strong>: 40</p>
 
 		<p><strong>Profession</strong>: </p>
 		<p><strong>Technology</strong>: </p>
