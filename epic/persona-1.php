9bc88d8bcad741f487fec36276c79e44f5edc263
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-31 16:06:54 -0700

    added persona for Jolynn to add to hers

--- epic/persona-1.php
+++ epic/persona-1.php
@@ -2,14 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Capstone persona-1</title>
+		<title>Feeding Our Past: persona-1</title>
 	</head>
 	<body>
-		<h1>Persona-1</h1>
-		<div>
+		<h1>Feeding Our Past: Persona-1</h1>
+		<!--<!-- Define a Persona representing the target audience for the capstone project:
+		This persona will be a person who is a senior-aged woman whose financial concerns have left her without food resources. -->
+		<p><strong>Name</strong>: Elizabeth Brown<br>
+			<strong>Age</strong>: 40</p>
 			This is Elizabeth.  She is a 67 year old female living in Albuquerque, New Mexico.
 		Her husband died several years ago after a long battle with cancer.
 		They never had children, so she has been on her own since then.
-		</div>
+
 	</body>
 </html>
--- epic/persona-2.php
+++ epic/persona-2.php
@@ -18,5 +18,40 @@
 		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot to quickly search for information online.</p>
 		<p><strong>Needs and Goals</strong>: Diane has recently learned that her elderly neighbor's wife died and he's now living alone with no family in town. She offers him meals when she is able to (maybe twice per week), but she worries he is not eating properly or getting out of the house to shop for food. She does not know his financial state and would like to find help for him, particularly a local food pantry to keep him from going hungry.</p>
 		<p><strong>User Story</strong>: As a user, I need to be able to quickly find food assistance in my area for my elderly neighbor.</p>
+
+		<p>Persona-1  Name: Elizabeth Brown
+			Age: 73
+
+			Family:  She is a Widower, no childen or extended family.
+
+			Profession: Elizabeth was a housewife as her husband had a good job.  She
+			lives on a fixed income, recieving a pension from her husband's company
+			plus her small SSI payment.
+
+			Technology:  Elizabeth is not computer literate, she is intimidated by the internet.  She has a windows 7 desktop computer that was given to her by her friend.
+			She also has an LG smartphone that Verizon talked her into that she can't really afford.
+			Her web browsing is mainly limited to finding coupons and passing time at gardening sites.
+			Elizabeth relies on her friendly neighbors to help her when she has a tech issue.
+
+			Attitudes and Behavior:  Elizabeth spends most of her days gardening, cleaning her house,
+			and trying to keep busy spending as little money as possible. She is very prideful, and will
+			not entertain the idea of an assisted living facility.  Her doctors have noticed a change in her
+			health and are concerned mainly about her diet. She has been putting on a brave face,
+			but six months ago her monthly income was not enough to cover her expenses and she
+			cut the only thing she felt she could, which was food. Her neighbors take her to the store once a week or so, and
+			as they noticed something was wrong they offered her help.  She declined the offer and became withdrawn, and now is only eating one small
+			meal a day.  She is concerned that the food pantry sites in Albuquerque are for homeless people only and would not help her, as well as being dangerous.
+			Elizabeth is scared.  She tried to search the internet for help with food, but found the websites presented were confusing
+			and only exascerbated her feelings of shame and desperation.
+
+
+			Needs and Goals:	Elizabeth knows she needs to do something, but with everything she is dealing with, she has found it hard to reach out.
+			Her needs are simply to find a way to not go hungry.  Her goal is to try and overcome her fear and find a way to recieve help.
+			She has no idea of who to contact, and won't admit to her neighbors she is in trouble, so the internet is really the only option for her.
+			She would like to take the first step to getting help by finding a church close to her that could help her.
+
+			User story:		As a user, I need to overcome both the negative feelings of adult hunger insecurity and the complications of bigger websites to
+			recieve the help I need.</p>
+
 	</body>
 </html>
