6ebc7de640d1a83f83a59d53db820580b7ad7728
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 20:19:28 -0700

    defined persona name, age, family, profession, and behavior

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -8,13 +8,15 @@
 		<h1>Feeding Our Past: Persona 2</h1>
 
 		<!-- Define a Persona representing the target audience for the capstone project:
-		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor -->
-		<p><strong>Name</strong>: Diane Baker<br>
-			<strong>Age</strong>: 40</p>
+		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor. -->
+		<p><strong>Name</strong>: Diane Smith<br>
+			<strong>Age</strong>: 40
+			<strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives nextdoor to an elderly (recent) widower, aged 75.</p>
 
-		<p><strong>Profession</strong>: </p>
-		<p><strong>Technology</strong>: </p>
-		<p><strong>Attitudes and Behaviors</strong>: </p>
+
+		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
+		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
+		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot.</p>
 		<p><strong>Needs and Goals</strong>: </p>
 		<p><strong>User Story</strong>: </p>
 	</body>
