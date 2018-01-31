728bd6c02046fc63f05788f5e9033e97dea3fa04
Peter Street <PeterBStreet@gmail.com>
2018-01-31 16:21:01 -0700

    Purpose cleaned out

--- epic/persona-3.php
+++ epic/persona-3.php
@@ -6,4 +6,10 @@
 	</head>
 	<body>
 		<h1>Feeding Our Past: Persona 3</h1>
+		<!-- Define a Persona representing the Community Engagement Partner for the capstone project:
+This persona will be an Community Engagement Partner (read organization) seeking information to help a senior-aged family member, friend, or neighbor. -->
+		
+		use case 
+		
+		process flow
 	</body>
