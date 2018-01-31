1fb939722234c6717cd61223ef48a908a25d9910
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 20:26:11 -0700

    defined persona needs and user story

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -11,13 +11,13 @@
 		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor. -->
 		<p><strong>Name</strong>: Diane Smith<br>
 			<strong>Age</strong>: 40
-			<strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives nextdoor to an elderly (recent) widower, aged 75.</p>
+			<strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives nextdoor to an elderly (recent) widower, aged 71.</p>
 
 
 		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
 		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
-		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot.</p>
-		<p><strong>Needs and Goals</strong>: </p>
-		<p><strong>User Story</strong>: </p>
+		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot to quickly search for information online.</p>
+		<p><strong>Needs and Goals</strong>: Diane has recently learned that her elderly neighbor's wife died and he's now living alone with no family in town. She offers him meals when she is able to (maybe twice per week), but she worries he is not eating properly or getting out of the house to shop for food. She does not know his financial state and would like to find help for him, particularly a local food pantry to keep him from going hungry.</p>
+		<p><strong>User Story</strong>: As a user, I need to be able to quickly find food assistance in my area for my elderly neighbor.</p>
 	</body>
 </html>
