9bf951d365a06423fd1474535237929ae752568c
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-31 12:44:00 -0700

    trying to fix display issues

--- epic/persona-1.php
+++ epic/persona-1.php
@@ -6,5 +6,10 @@
 	</head>
 	<body>
 		<h1>Persona-1</h1>
+		<div>
+			This is Elizabeth.  She is a 67 year old female living in Albuquerque, New Mexico.
+		Her husband died several years ago after a long battle with cancer.
+		They never had children, so she has been on her own since then.
+		</div>
 	</body>
 </html>
