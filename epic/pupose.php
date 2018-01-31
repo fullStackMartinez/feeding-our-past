6f96062ae782ccebbe10eb0619326a4153f7aeb9
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-31 15:02:40 -0700

    lets try this again

--- epic/pupose.php
+++ epic/pupose.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: petersdata
- * Date: 1/30/18
- * Time: 12:27 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Purpose Statement</title>
+	</head>
+	<body>
+		<h1>Purpose Statement</h1>
+		<h3>Feeding Our Past</h3>
+		<p>The purpose of our Capstone project is to build a website that easily puts the elderly and their families in touch with local nutritional resources here in Albuquerque.</p>
+		<p>Our website will serve as an informational hub where elderly people and their families can not only search and find these resources, but also suggest new resources in order to build this as a pillar of our community. Taking car of those who took care of us. This is our aim.</p>
+	</body>
+</html>
