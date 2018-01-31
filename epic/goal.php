5ff563620f229bdb2f4b57b9a13a71dff4a49490
Peter Street <PeterBStreet@gmail.com>
2018-01-31 12:00:59 -0700

    Peter's goal c/p

--- epic/goal.php
+++ epic/goal.php
@@ -6,8 +6,8 @@
  * Time: 12:27 PM
  */
 
-/*    Purpose
- The purpose of the "Feeding Our Past" CapStone project is to provide Albuquerque NM seniors food resources. The Road Runner Food Bank of New Mexico suscintly outlines the need for "Feeding Our Past" stating, "Senior have said to us one of the first expenses they cut from their monthly expenses is food...seniors, limiting their food intake can drastically alter their health and significantly impact their quality of life." It seems likely, that seniors choosing to cut their food intake to make ends meet do not have access to internet services.  Family, friends, and neighbors of these at risk seniors need an easy informational website/application listing resources to meet the unique needs of Albuquerque's seniors. In most cases Albuquerque's food banks/pantries offer additional services such as assistance with trash collection and water bills.
+/*    Goal
+ The Goal of the "Feeding Our Past" CapStone project is to provide Albuquerque NM seniors food resources. The Road Runner Food Bank of New Mexico suscintly outlines the need for "Feeding Our Past" stating, "Senior have said to us one of the first expenses they cut from their monthly expenses is food...seniors, limiting their food intake can drastically alter their health and significantly impact their quality of life." It seems likely, that seniors choosing to cut their food intake to make ends meet do not have access to internet services.  Family, friends, and neighbors of these at risk seniors need an easy informational website/application listing resources to meet the unique needs of Albuquerque's seniors. In most cases Albuquerque's food banks/pantries offer additional services such as assistance with trash collection and water bills.
 
 
     Sources
