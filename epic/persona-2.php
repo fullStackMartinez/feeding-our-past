a18b4a21779efb362b833e039b7fea9b672c23ae
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 20:31:35 -0700

    fixed line spacing

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -10,10 +10,9 @@
 		<!-- Define a Persona representing the target audience for the capstone project:
 		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor. -->
 		<p><strong>Name</strong>: Diane Smith<br>
-			<strong>Age</strong>: 40
-			<strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives nextdoor to an elderly (recent) widower, aged 71.</p>
+			<strong>Age</strong>: 40</p>
 
-
+		<p><strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives next door to an elderly widower, aged 71.</p>
 		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
 		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
 		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot to quickly search for information online.</p>
