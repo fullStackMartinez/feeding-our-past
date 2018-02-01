3f536b044023d7a02394f98b1dcf02a2524fb44b
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-01 10:01:53 -0700

    added interaction flow and frequency of use to volunteer persona-1

--- epic/persona-1.php
+++ epic/persona-1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8" />
 		<title>Feeding Our Past: Persona-1</title>
 	</head>
 	<body>
@@ -63,7 +63,21 @@
 			<h3>Frequency of Use</h3>
 			<p>Andrew is a one time volunteer, meaning he is using our web site/app in order to take care of a one time drop off of donated food. We plan on having two types of volunteers, those that are one time volunteers, and those that want to continue to help in the future, many times a year.</p>
 		</section>
-		<!--
+		<!--Since we have 2 volunteer types, I will edit the above Persona, Andrew Candelaria, as a volunteer who wants to continue volunteering in Albuquerque long term, and so makes a profile on our web site/ app-->
+		<section>
+				<h3>Andrew Candelaria</h3>
+				<p>Age: 24 years old</p>
+				<p>Lives in the Nob Hill area of Albuquerque, New Mexico</p>
+				<p>Profession: Recent college grad who just got a job as a financial advisor for a bank in Downtown Albuquerque.</p>
+				<p>Technology: Andrew owns a laptop (PC) and a mobile smart phone (Iphone 6). He also owns a tablet that is a few years old that he rarely uses.
+					He is proficient in using all three, knows how to download an app and use it, load a web page and browse the internet. He uses Chrome as his browser and texts as his main form of communication. Whenever he has a question for something he needs/wants to know, he uses Google.
+				</p>
+				<p>Attitude and Behaviors: Andrew is a recent UNM graduate, having worked hard for 5 years studying full time, while working a part time job that took most of his weekends/free time. The past 2 years he has been living with 2 friends, both of which are similar in age and already in the work force.
+					Andrew's new life is busy, he is a hard worker, with a new job he is learning the ropes, taking any free time to study his employee handbook and also moving into a new apartment.
+				</p>
+				<p>Needs: Andrew grew up in poverty, and now that he has a good job and security he wants to give back to the community. He would like to find an informational hub on volunteering to feed the elderly
+				</p>
+		</section>
 
 	</body>
 </html>
