40c25baef81fd59b38e5a0379f0116cf7786b1e0
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-31 20:31:12 -0700

    added note about user story

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -18,25 +18,25 @@
 
 		<p><strong>Profession</strong>: Elizabeth was a housewife as her husband had a good job. She lives on a fixed income, receiving a pension from her husband's company plus her small SSI payment.</p>
 
-		<p><strong>Technology</strong>: Elizabeth is not computer literate, she is intimidated by the internet.  She has a Windows 7 desktop computer that was given to her by her friend. She also has an LG smartphone that Verizon talked her into that she can't really afford. Her web browsing is mainly limited to finding coupons and passing time at gardening sites. Elizabeth relies on her friendly neighbors to help her when she has a tech issue.</p>
+		<p><strong>Technology</strong>: Elizabeth has minimal computer skills and is intimidated by the internet.  She has a Windows 7 desktop computer that was given to her by her friend. She also has an LG smartphone that Verizon talked her into that she can't really afford. Her web browsing is mainly limited to finding coupons and passing time at gardening sites. Elizabeth relies on her friendly neighbors to help her when she has a tech issue.</p>
 
 		<p><strong>Attitudes and Behavior</strong>: Elizabeth spends most of her days gardening, cleaning her house, and trying to keep busy spending as little money as possible. She is very prideful, and will not entertain the idea of an assisted living facility.  Her doctors have noticed a change in her health and are concerned mainly about her diet. She has been putting on a brave face, but six months ago her monthly income was not enough to cover her expenses and she cut the only thing she felt she could, which was food. Her neighbors take her to the store once a week or so, and as they noticed something was wrong they offered her help.  She declined the offer and became withdrawn, and now is only eating one small meal a day. She is concerned that the food pantry sites in Albuquerque are for homeless people only and would not help her, and she feels those sites are unsafe. Elizabeth is scared. She tried to search the internet for help with food, but found the websites presented were confusing and only exacerbated her feelings of shame and desperation.</p>
 
 		<p><strong>Needs and Goals</strong>: Elizabeth knows she needs to do something, but with everything she is dealing with, she has found it hard to reach out. Her needs are simply to find a way to not go hungry. Her goal is to try and overcome her fear and find a way to receive help. She has no idea who to contact and won't admit to her neighbors she is in trouble, so the internet is really the only option for her. She would like to take the first step to getting help by finding a church or organization nearby that could help her. Elizabeth needs a simple, user-friendly interaction with the website to find the information she is looking for.</p>
 
 <!--		<p><strong>User story</strong>: As a user, I need to overcome both the negative feelings of adult hunger insecurity and the complications of bigger websites to receive the help I need.</p> -->
-		<p><strong>User Story</strong>: As a user, I need to be able to easily find food assistance in my area and see results in a simple, easy to navigate manner.</p>
+		<p><strong>User Story*</strong>: As a user, I need to be able to easily find food assistance in my area and see results in a simple, easy to navigate manner.</p>
 
 		<h2>Use Case and Interaction Flow</h2>
 
 		<!-- Define this persona's use case and interaction flow -->
 		<p>As a user, Elizabeth wants to easily find information about organizations near her who assist with food insecurity. She wants to see results in a clear, easy to read format.</p>
 
-		<p>Usage precondition: Elizabeth navigated to the website and knows her address.</p>
+		<p><strong>Usage precondition</strong>: Elizabeth navigated to the website and knows her address.</p>
 
-		<p>Usage postcondition: Elizabeth has a list of nearby organizations to help her, and she can easily see them on a map relative to her location.</p>
+		<p><strong>Usage postcondition</strong>: Elizabeth has a list of nearby organizations to help her, and she can easily see them on a map relative to her location.</p>
 
-		<p>Interaction flow:</p>
+		<p><strong>Interaction flow:</strong></p>
 		<ol>
 			<li>Elizabeth types in the website's URL</li>
 			<li>The site opens at the simple, user-friendly homepage</li>
@@ -46,10 +46,12 @@
 			<li>The site returns a list of organizations and displays their locations on a Google map</li>
 		</ol>
 
+		<p><br><strong>*Note about User Story</strong>: If Elizabeth had been open to asking her neighbor for help in finding food assistance, this User Story would also apply to the neighbor, as would the Use Case and Interaction Flow that followed. This is noted here to point out that family, friends, neighbors, helpers would be able to search for and access information in the same manner a senior citizen would. This grouping of people would be "seeking assistance."
 
-<!--		<p><strong>Name</strong>: Diane Smith<br>
-			<strong>Age</strong>: 40</p>
 
+<!-- An example of another persona who would be seeking assistance
+		<p><strong>Name</strong>: Diane Smith<br>
+		<strong>Age</strong>: 40</p>
 		<p><strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives next door to an elderly widower, aged 71.</p>
 		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
 		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
