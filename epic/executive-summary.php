43ac2c054d6f1bc27553fe63a43af0e3ce3f6f83
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-31 21:31:25 -0700

    created executive summary document, added title and team to index.html

---
+++ epic/executive-summary.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Executive Summary</title>
+	</head>
+	<body>
+		<h1>Capstone: Stop Senior Hunger</h1>
+		<h2>Executive Summary</h2>
+		<h3>Team: Esteban, Jeff, Jolynn, Peter</h3>
+
+	</body>
+</html>
