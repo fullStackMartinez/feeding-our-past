e835c841f7fe33a4c9dbc38323ba85861c6dc440
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-01 08:35:04 -0700

    deleted pupose

--- epic/persona-1.php
+++ epic/persona-1.php
@@ -17,6 +17,11 @@
 			<p>Technology: Andrew owns a laptop (PC) and a mobile smart phone (Iphone 6). He also owns a tablet that is a few years old that he rarely uses.
 				He is proficient in using all three, knows how to download an app and use it, load a web page and browse the internet. He uses Chrome as his browser and texts as his main form of communication. Whenever he has a question for something he needs/wants to know, he uses Google.
 			</p>
+			<p>Attitude and Behaviors: Andrew is a recent UNM graduate, having worked hard for 5 years studying full time, while working a part time job that took most of his weekends/free time. The past 2 years he has been living with 2 friends, both of which are similar in age and already in the work force.
+				Andrew's new life is busy, he is a hard worker, with a new job he is learning the ropes, taking any free time to study his employee handbook and also moving into a new apartment.
+			</p>
+			<p>Needs: Andrew has a lot of things to do on his plate, including cleaning out the rental property he and his friends are about to vacate. He needs to find a place close to him to donate all the extra perishables he doesn't intend on using, that accepts food donations and is open when he has free time.
+			</p>
 		</section>
 
 	</body>
