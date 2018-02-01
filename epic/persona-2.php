ec8b34a12af20c4e7cea929e222ee0c707001aac
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-31 19:53:13 -0700

    edited persona 2 content

--- epic/persona-2.php
+++ epic/persona-2.php
@@ -6,45 +6,21 @@
 	</head>
 	<body>
 		<h1>Feeding Our Past: Persona 2</h1>
-		<h2>"People Seeking Assistance"</h2>
+		<h2>"Person Seeking Assistance"</h2>
 
 		<!-- Define a Persona representing the target audience for the capstone project:
-		This persona will be a person who is seeking information to help a senior-aged family member, friend, or neighbor. -->
-		<p><strong>Name</strong>: Diane Smith<br>
-			<strong>Age</strong>: 40</p>
+		This persona will be a senior citizen who is seeking help for herself. -->
+		<p><strong>Name</strong>: Elizabeth Brown</p>
 
-		<p><strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives next door to an elderly widower, aged 71.</p>
-		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
-		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
-		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot to quickly search for information online.</p>
-		<p><strong>Needs and Goals</strong>: Diane has recently learned that her elderly neighbor's wife died and he's now living alone with no family in town. She offers him meals when she is able to (maybe twice per week), but she worries he is not eating properly or getting out of the house to shop for food. She does not know his financial state and would like to find help for him, particularly a local food pantry to keep him from going hungry.</p>
-		<p><strong>User Story</strong>: As a user, I need to be able to quickly find food assistance in my area for my elderly neighbor.</p>
+		<p><strong>Age</strong>: 73</p>
 
-		<p>Persona-1  Name: Elizabeth Brown
-			Age: 73
+		<p><strong>Family</strong>: She is a Widow, no childen or extended family.</p>
 
-			Family:  She is a Widow, no childen or extended family.
+		<p><strong>Profession</strong>: Elizabeth was a housewife as her husband had a good job. She lives on a fixed income, receiving a pension from her husband's company plus her small SSI payment.</p>
 
-			Profession: Elizabeth was a housewife as her husband had a good job.  She
-			lives on a fixed income, recieving a pension from her husband's company
-			plus her small SSI payment.
+		<p><strong>Technology</strong>: Elizabeth is not computer literate, she is intimidated by the internet.  She has a Windows 7 desktop computer that was given to her by her friend. She also has an LG smartphone that Verizon talked her into that she can't really afford. Her web browsing is mainly limited to finding coupons and passing time at gardening sites. Elizabeth relies on her friendly neighbors to help her when she has a tech issue.</p>
 
-			Technology:  Elizabeth is not computer literate, she is intimidated by the internet.  She has a windows 7 desktop computer that was given to her by her friend.
-			She also has an LG smartphone that Verizon talked her into that she can't really afford.
-			Her web browsing is mainly limited to finding coupons and passing time at gardening sites.
-			Elizabeth relies on her friendly neighbors to help her when she has a tech issue.
-
-			Attitudes and Behavior:  Elizabeth spends most of her days gardening, cleaning her house,
-			and trying to keep busy spending as little money as possible. She is very prideful, and will
-			not entertain the idea of an assisted living facility.  Her doctors have noticed a change in her
-			health and are concerned mainly about her diet. She has been putting on a brave face,
-			but six months ago her monthly income was not enough to cover her expenses and she
-			cut the only thing she felt she could, which was food. Her neighbors take her to the store once a week or so, and
-			as they noticed something was wrong they offered her help.  She declined the offer and became withdrawn, and now is only eating one small
-			meal a day.  She is concerned that the food pantry sites in Albuquerque are for homeless people only and would not help her, as well as being dangerous.
-			Elizabeth is scared.  She tried to search the internet for help with food, but found the websites presented were confusing
-			and only exascerbated her feelings of shame and desperation.
-
+		<p><strong>Attitudes and Behavior</strong>: Elizabeth spends most of her days gardening, cleaning her house, and trying to keep busy spending as little money as possible. She is very prideful, and will not entertain the idea of an assisted living facility.  Her doctors have noticed a change in her health and are concerned mainly about her diet. She has been putting on a brave face, but six months ago her monthly income was not enough to cover her expenses and she cut the only thing she felt she could, which was food. Her neighbors take her to the store once a week or so, and as they noticed something was wrong they offered her help.  She declined the offer and became withdrawn, and now is only eating one small meal a day. She is concerned that the food pantry sites in Albuquerque are for homeless people only and would not help her, and she feels those sites are unsafe. Elizabeth is scared. She tried to search the internet for help with food, but found the websites presented were confusing and only exacerbated her feelings of shame and desperation.</p>
 
 			Needs and Goals:	Elizabeth knows she needs to do something, but with everything she is dealing with, she has found it hard to reach out.
 			Her needs are simply to find a way to not go hungry.  Her goal is to try and overcome her fear and find a way to recieve help.
@@ -77,5 +53,15 @@
 		</ol>
 -->
 
+<!--		<p><strong>Name</strong>: Diane Smith<br>
+			<strong>Age</strong>: 40</p>
+
+		<p><strong>Family</strong>: Married with 2 children, 8 and 10 years old. The family lives next door to an elderly widower, aged 71.</p>
+		<p><strong>Profession</strong>: Elementary school teacher, she teaches 4th grade.</p>
+		<p><strong>Technology</strong>: At home, a Mac mini with a 21.5" display and an iPhone 7. At work, a Dell laptop running Windows 10 and a Google Chromebook at times.</p>
+		<p><strong>Attitudes and Behaviors</strong>: Diane is tired and busy with her full-time teaching job during the day, and then when not shuttling her kids to extracurricular activities in the evening, she is home preparing dinner, catching up on housework, helping her kids with homework, and grading her student's schoolwork. She doesn't get a lot of time on her computer at home and relies on her phone a lot to quickly search for information online.</p>
+		<p><strong>Needs and Goals</strong>: Diane has recently learned that her elderly neighbor's wife died and he's now living alone with no family in town. She offers him meals when she is able to (maybe twice per week), but she worries he is not eating properly or getting out of the house to shop for food. She does not know his financial state and would like to find help for him, particularly a local food pantry to keep him from going hungry.</p>
+		<p><strong>User Story</strong>: As a user, I need to be able to quickly find food assistance in my area for my elderly neighbor.</p>
+-->
 	</body>
 </html>
