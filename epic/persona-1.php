380729384a9b4dec172f53fbc688db0b4a754de1
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-01 08:32:20 -0700

    deleted pupose

--- epic/persona-1.php
+++ epic/persona-1.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8"/>
-		<title>Feeding Our Past: persona-1</title>
+		<meta charset="UTF-8">
+		<title>Feeding Our Past: Persona-1</title>
 	</head>
 	<body>
-		<h1>Feeding Our Past: Persona-1</h1>
-		<!--<!-- Define a Persona representing the target audience for the capstone project:
-		This persona will be a person who is a senior-aged woman whose financial concerns have left her without food resources. -->
-		<p><strong>Name</strong>: Elizabeth Brown<br>
-			<strong>Age</strong>: 40</p>
-			This is Elizabeth.  She is a 67 year old female living in Albuquerque, New Mexico.
-		Her husband died several years ago after a long battle with cancer.
-		They never had children, so she has been on her own since then.
+		<header>
+			<h2>The Volunteer</h2>
+			<!--Here we will define a the Volunteer user of our app/web page-->
+		</header>
+		<section>
+			<h3>Andrew Candelaria</h3>
+			<p>Age: 24 years old</p>
+			<p>Lives in the Nob Hill area of Albuquerque, New Mexico</p>
+			<p>Profession: Recent college grad who just got a job as a financial advisor for a bank in Downtown Albuquerque.</p>
+			<p>Technology: Andrew owns a laptop (PC) and a mobile smart phone (Iphone 6). He also owns a tablet that is a few years old that he rarely uses.
+				He is proficient in using all three, knows how to download an app and use it, load a web page and browse the internet. He uses Chrome as his browser and texts as his main form of communication. Whenever he has a question for something he needs/wants to know, he uses Google.
+			</p>
+		</section>
 
 	</body>
 </html>
