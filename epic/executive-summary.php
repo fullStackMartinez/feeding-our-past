4d9c03663210a488dc58487746d5f58f144fe6e0
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-31 21:41:52 -0700

    edited title, ready for content

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -5,9 +5,8 @@
 		<title>Executive Summary</title>
 	</head>
 	<body>
-		<h1>Capstone: Stop Senior Hunger</h1>
-		<h2>Executive Summary</h2>
-		<h3>Team: Esteban, Jeff, Jolynn, Peter</h3>
+		<h1>Feeding Our Past: Executive Summary</h1>
+
 
 	</body>
 </html>
