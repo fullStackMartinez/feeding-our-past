1b52103a7af88f2a8f97ce3a13ac0513ca7ac0bc
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-05 15:27:05 -0700

    started the Organization.php class

--- php/classes/Organization.php
+++ php/classes/Organization.php
@@ -1,7 +1,31 @@
 <?php
+namespace Edu\Cnm\DataDesign;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__,2) . "/vendo/autoload.php");
+
+
+
+use Ramsey\Uuid\Uuid;
+
 /**
- * Created by PhpStorm.
- * User: Esteban
- * Date: 2/5/2018
- * Time: 2:37 PM
- */
+ * This is the organization class
+ *
+ * This class is the organization that will be registering with our website writing posts in order to market their events. Their information will
+ * be stored and they will have the ability to write posts
+ *
+ * @author Esteban Martinez <fullstackmartinez@gmail.com>
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ * @version 1.0.0
+ **/
+class Organization implements \JsonSerializable {
+	use ValidateUuid;
+	/**
+	 * organization id, this will serve as the primary key for this class
+	 * @var Uuid $organizationId
+	 **/
+	private $organizationId;
+	/**
+	 *This is the activation token which allows organizations to complete registration
+	 */
+}
