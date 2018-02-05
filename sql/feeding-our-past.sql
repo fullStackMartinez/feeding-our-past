5d6dcdb41cbd67f38e10668d7795a1c06c2e8812
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-05 13:45:08 -0700

    added create table post
    https://app.asana.com/0/543064250979533/548451296418338

--- sql/feeding-our-past.sql
+++ sql/feeding-our-past.sql
@@ -44,3 +44,21 @@ CREATE TABLE volunteer (
 	-- primary key is volunteerId
 	PRIMARY KEY(volunteerId)
 );
+
+CREATE TABLE post (
+	-- this is the primary key
+	postId BINARY(16) NOT NULL,
+	-- this is the foreign key
+	postOrganizationId BINARY(16) NOT NULL,
+	postContent VARCHAR(12000) NOT NULL,
+	postEndDateTime DATETIME(6) NOT NULL,
+	postImageUrl VARCHAR(255),
+	postStartDateTime DATETIME(6) NOT NULL,
+	postTitle VARCHAR(255) NOT NULL,
+	-- create index before making a foreign key
+	INDEX(postOrganizationId),
+	-- create the actual foreign key relation
+	FOREIGN KEY(postOrganizationId) REFERENCES organization(organizationId),
+	-- create the primary key
+	PRIMARY KEY(postId)
+);
