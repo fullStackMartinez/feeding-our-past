62a41f928a360f9559a03af55260b303ae9bc517
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-05 14:58:36 -0700

    added jSonSerialize function and beginning of Volunteer class
    https://app.asana.com/0/543064250979533/548451296418338

--- php/classes/Volunteer.php
+++ php/classes/Volunteer.php
@@ -5,3 +5,39 @@ require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
+
+/**
+ * Volunteer class for capstone project
+ *
+ * This is a top level entity for Volunteer in the capstone project
+ *
+ * @author Jolynn Pruitt <jpruitt5@cnm.edu>
+ **/
+class Volunteer implements \JsonSerializable {
+	use ValidateUuid;
+
+	/**
+	 * id for this Volunteer; this is the primary key
+	 * @var Uuid $volunteerId
+	 **/
+	private $volunteerId;
+
+
+
+
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+
+		$fields["volunteerId"] = $this->volunteerId->toString();
+		unset($fields["volunteerActivationToken"]);
+		unset($fields["volunteerHash"]);
+		unset($fields["volunteerSalt"]);
+
+		return($fields);
+	}
+}
