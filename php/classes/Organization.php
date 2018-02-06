3b27a635bd95d82908f72e10983a4d546f50c353
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-06 13:33:10 -0700

    wrote all private state variables for Organization.php https://app.asana.com/0/543064250979533/543114592177805

--- php/classes/Organization.php
+++ php/classes/Organization.php
@@ -21,29 +21,81 @@ use Ramsey\Uuid\Uuid;
 class Organization implements \JsonSerializable {
 	use ValidateUuid;
 	/**
-	 * organization id, this will serve as the primary key for this class
+	 * this is organization id, this will serve as the primary key for this class
 	 * @var Uuid $organizationId
 	 **/
 	private $organizationId;
 	/**
-	 *This is the activation token which allows organizations to complete their registration
+	 * this is the activation token which allows organizations to validate the organization profile and security
 	 * @var $organizationActivationToken
 	 **/
 	private $organizationActivationToken;
 	/**
-	 *organization city, here will be the city of the organization
+	 * this is the organization city, here will be the city of the organization
 	 * @var string $organizationAddressCity
 	 **/
 	private $organizationAddressCity;
 	/**
-	 * organization state, the state that the city is a part of for the organization
+	 * this is the organization state, the state that the city is a part of for the organization
 	 * @var string $organizationAddressState
 	 **/
 	private $organizationAddressState;
 	/**
-	 * organization street, the street address of the organization
+	 * this is the organization street, the street address of the organization
 	 * @var string $organizationAddressStreet
 	 **/
 	private $organizationAddressStreet;
+	/**
+	 * this is the organization zip code, the official mailing zip code of the registered organization profile
+	 * @var string $organizationAddressZip;
+	 **/
+	private $organizationAddressZip;
+	/**
+	 * this state variable will determine if the organization accepts food donations
+	 * @var string $organizationDonationAccepted
+	 **/
+	private $organizationDonationAccepted;
+	/**
+	 * this is the organizations email address used for communication, this is unique
+	 * @var string @organizationEmail
+	 **/
+	private $organizationEmail;
+	/**
+	 * this is the hash for the organization profile password
+	 * @var $organizationHash
+	 **/
+	private $organizationHash;
+	/**
+	 * this will determine the operating hours of the organization
+	 * @var string $organizationHoursOpen
+	 **/
+	private $organizationHoursOpen;
+	/**
+	 * this is the official display name of the organization
+	 * @var string $organizationName
+	 **/
+	private $organizationName;
+	/**
+	 * this is the contact phone number of the organization
+	 * @var string $organizationPhone;
+	 **/
+	private $organizationPhone;
+	/**
+	 * this is the salt for password
+	 * @var $organizationSalt;
+	 **/
+	private $organizationSalt;
+	/**
+	 * this is the URL of the organization, this is unique
+	 * @var string $organizationUrl
+	 */
+	private $organizationUrl;
+
+	/**
+	 * constructor for this Organization
+	 *
+	 * @param string|Uuid $newOrganizationId will give id of the organizations profile, or null if a brand new profile
+	 * @param string
+	 */
 
 }
