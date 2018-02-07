6485a5a3c7e4b0756df32711df1e781a50f0966e
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-07 13:56:44 -0700

    created docblock for VolunteerTest
    https://app.asana.com/0/543064250979533/548451296418335

--- php/classes/Test/VolunteerTest.php
+++ php/classes/Test/VolunteerTest.php
@@ -1,4 +1,21 @@
 <?php
 namespace Edu\Cnm\FeedPast;
 
-// Unit Test for Volunteer
+use Edu\Cnm\FeedPast\Volunteer;
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the Volunteer class
+ *
+ * This is a complete PHPUnit test of the Volunteer class. It is complete because all mySQL/PDO methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Volunteer
+ * @author Jolynn Pruitt <jpruitt5@cnm.edu>
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ **/
