08f7e7115c20c196bb32b0a8778b778d89cface6
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-08 12:54:16 -0700

    added favoritetest.php
    https://app.asana.com/0/543064247470558/548451296418336

--- php/classes/Test/FavoriteTest.php
+++ php/classes/Test/FavoriteTest.php
@@ -4,12 +4,24 @@ namespace Edu\Cnm\Feedpast\Test;
 use Edu\Cnm\feedpast\{Favorite, Organization, Volunteer};
 
 // grab the class under scrunity
-require_once(dirname(__DIR__,2) . "/autoload.php");
+require_once(dirname(__DIR__,1) . "/autoload.php");
 
 // grab the uuid generator
-require_once(dirname(__DIR__,) . "lib/uuid.php");
+require_once(dirname(__DIR__,2) . "/lib/uuid.php");
 
 /**
- * Full PhpUnit test for the favorites class.  It is complete because ALL mySQL/PDO enabled methods
+ * Full PhpUnit test for the favorite class.
+ *
+ * It is complete because ALL mySQL/PDO enabled methods
  * are tested for both valid and invalid inputs.
- */
+ * @see Favorite
+ * @author Jeffrey Brink <jeffreybrink@gmx.com
+ * @author Dylan McDonald <dmcdonald@cnm.edu>
+ **/
+class FavoriteTest extends feedpasttest {
+
+	/**
+	 * Profile that created the
+	 */
+
+}
