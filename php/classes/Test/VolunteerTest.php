4235781ce36b5ab9ab7151f1a69ebd9f3344a478
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-09 10:00:58 -0700

    fixed path to uuid.php file
    https://app.asana.com/0/543064250979533/548451296418335

--- php/classes/Test/VolunteerTest.php
+++ php/classes/Test/VolunteerTest.php
@@ -7,7 +7,8 @@ use Edu\Cnm\FeedPast\Volunteer;
 require_once(dirname(__DIR__) . "/autoload.php");
 
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+//require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+require_once("../../lib/uuid.php");
 
 /**
  * Full PHPUnit test for the Volunteer class
