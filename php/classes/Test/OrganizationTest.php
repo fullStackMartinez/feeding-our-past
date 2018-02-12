b8ca0180a36c170d106eaa3182d75a36781a57ea
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-12 11:06:19 -0700

    typo fixed on line 213 of org.php  https://app.asana.com/0/543064250979533/543114592177805

--- php/classes/Test/OrganizationTest.php
+++ php/classes/Test/OrganizationTest.php
@@ -310,7 +310,7 @@ class OrganizationTest extends FeedPastTest {
 		$organization = new Organization($organizationId, $this->VALID_ACTIVATION, $this->VALID_ADDRESS_CITY, $this->VALID_ADDRESS_STATE, $this->VALID_ADDRESS_STREET, $this->VALID_ADDRESS_ZIP, $this->VALID_DONATION_ACCEPTED, $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_HOURS_OPEN, $this->VALID_LAT, $this->VALID_LONG, $this->VALID_NAME, $this->VALID_PHONE, $this->VALID_SALT, $this->VALID_URL);
 		$organization->insert($this->getPDO());
 		//grab data from database
-		$results = Organization::getOrganizationByDistance($this->getPDO());
+		$results = Organization::getOrganizationByDistance($this->getPDO(), 45, 45, .00001);
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("organization"));
 
 		//make sure there is no overlapping of objects in organization
