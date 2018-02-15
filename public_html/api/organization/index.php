48bf53c91d9c6bbb31906638b78ad3ad51c3d814
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-15 15:24:16 -0700

    started organization API with require_once and verifying session https://app.asana.com/0/543064250979533/560341220837896

--- public_html/api/organization/index.php
+++ public_html/api/organization/index.php
@@ -1,7 +1,31 @@
 <?php
+
+
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\FeedPast\ {
+	Organization
+};
+
 /**
- * Created by PhpStorm.
- * User: Esteban
- * Date: 2/15/2018
- * Time: 3:05 PM
- */
+ * API for Organization
+ *
+ * @author Esteban Martinez
+ * @author George Kephart
+ * @version 1.0
+ **/
+
+//verify the session, and if the session isn't active, go ahead and start it
+if(session_status() !==PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//set up an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
