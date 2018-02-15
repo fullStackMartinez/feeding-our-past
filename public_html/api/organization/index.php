78999352f4ea44487389953201ec5e66ac836b15
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-15 16:30:58 -0700

    wrote lines 54-68 get organization by ID https://app.asana.com/0/543064250979533/560341220837896

--- public_html/api/organization/index.php
+++ public_html/api/organization/index.php
@@ -29,3 +29,45 @@ if(session_status() !==PHP_SESSION_ACTIVE) {
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+try {
+	//secure the MySQL connection to our database
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+
+	//determine the HTTP method
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$organizationEmail = filter_input(INPUT_GET, "organizationEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$organizationName = filter_input(INPUT_GET, "organizationName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("sorry but your ID cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+		//set the XSRF cookie
+		setXsrfCookie();
+
+		//gets an organization starting by their id and if not, the next method
+		if(empty($id) === false) {
+			$organization = Organization::getOrganizationByOrganizationId($pdo, $id);
+			if($organization !== null) {
+				$reply->data = $organization;
+			}
+		} else if(empty($organizationEmail) === false) {
+			$organization = Organization::getOrganizationByOrganizationEmail($pdo, $organizationEmail);
+			if($organization !== null) {
+				$reply->data = $organization;
+			}
+		} else if(empty($organizationName) === false) {
+			$organization = Organization::getOrganizationByOrganizationName($pdo, $organizationName);
+			if($organization !== null) {
+				$reply->data = $organization;
+			}
+		}
+	}
+
+}
