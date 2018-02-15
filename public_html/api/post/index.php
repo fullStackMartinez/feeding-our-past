b68fa591396c3e1155e694e978f6bbbded8e5df3
Peter Street <PeterBStreet@gmail.com>
2018-02-15 15:30:42 -0700

    Created directories and files feeding-our-past/public_html/post/.htaccess feeding-our-past/public_html/sign-out/.htaccess feeding-our-past/public_html/post/index.php feeding-our-past/public_html/sign-out/index.php
    post
    https://app.asana.com/0/543064250979533/560341220837891
    
    signout
    https://app.asana.com/0/543064250979533/560341220837893

---
+++ public_html/api/post/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petersdata
+ * Date: 2/15/18
+ * Time: 3:18 PM
+ */
---
+++ public_html/api/sign-out/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petersdata
+ * Date: 2/15/18
+ * Time: 3:17 PM
+ */
