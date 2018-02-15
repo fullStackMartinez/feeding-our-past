675ab94ad7593534109b141c46d150f1f0200d4a
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-15 13:51:39 -0700

    verify session, prepare empty reply
    https://app.asana.com/0/543064250979533/560341220837897

--- public_html/api/volunteer/index.php
+++ public_html/api/volunteer/index.php
@@ -10,4 +10,19 @@ require_once("etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\FeedPast\ {Volunteer};
 
+/**
+ * API for Volunteer
+ *
+ * @author Jolynn Pruitt <jpruitt5@cnm.edu>
+ * @author Gkephart
+ */
+
+// verify the session, start it if not already active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
 
