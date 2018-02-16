fec04cf261a516cc25bf1d70fdc90a3366a88817
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-16 11:03:23 -0700

    changed namespace and fixed docblock
    https://app.asana.com/0/543064250979533/560341220837897

--- public_html/api/volunteer/index.php
+++ public_html/api/volunteer/index.php
@@ -8,14 +8,14 @@ require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 
 require_once("etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\FeedPast\ {Volunteer};
+use Edu\Cnm\FeedPast\Volunteer;
 
 /**
  * API for Volunteer
  *
  * @author Jolynn Pruitt <jpruitt5@cnm.edu>
  * @author Gkephart
- */
+ **/
 
 // verify the session, start it if not already active
 if(session_status() !== PHP_SESSION_ACTIVE) {
