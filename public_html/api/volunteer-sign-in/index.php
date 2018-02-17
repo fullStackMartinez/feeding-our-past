4d63f273fe4c2b59a7f262baa8819acee82daca0
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-16 20:18:58 -0700

    verify the session, grab mysql statement, determine HTTP method
    https://app.asana.com/0/543064250979533/560341220837892

--- public_html/api/volunteer-sign-in/index.php
+++ public_html/api/volunteer-sign-in/index.php
@@ -6,3 +6,46 @@ require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\FeedPast\Volunteer;
 
+/**
+ * API to handle volunteer sign-in
+ *
+ * @author Jolynn Pruitt <jpruitt5@cnm.edu>
+ * @author Gkephart
+ **/
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	// verify the session, start if not active
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/feedkitty.ini");
+
+	// determine which HTTP method was used
+	// shorthand: $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if(array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER)) {
+		$method = $_SERVER["HTTP_X_HTTP_METHOD"];
+	} else {
+		$method = $_SERVER["REQUEST_METHOD"];
+	}
+
+	if($method === "POST") {
+
+		// make sure the XSRF Token is valid
+		verifyXsrf();
+
+		// process the request content and decode the json object into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+
+
+	}
+
+}
