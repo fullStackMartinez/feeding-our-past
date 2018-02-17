b4c8eafa02ad931b013d26d2ebb3c461ac5bedcc
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-17 15:33:48 -0700

    working on favorite api favorite api                                                        https://app.asana.com/0/543064247470558/list

--- public_html/api/favorite/index.php
+++ public_html/api/favorite/index.php
@@ -58,9 +58,98 @@ try {
 			}
 
 			//get all the favorites associated with the post Id
-		} else if(empty($favoritePostId) === false) {
-			$favorite= Favorite::getFavoriteByFavoritePostId($pdo, $favoritePostId)->toArray();
+		   } else if(empty($favoritePostId) === false) {
+			$favorite = Favorite::getFavoriteByFavoritePostId($pdo, $favoritePostId)->toArray();
+
 			if($favorite !== null) {
 				$reply->data = $favorite;
 			}
+		} else {
+					throw new InvalidArgumentException("incorrect search parameters ", 404);
 		}
+
+
+	} else if($method === "POST" || $method === "PUT") {
+
+				//decide the response from the front end
+				$requestContent = file_get_contents("php://input");
+				$requestObject = json_decode($requestContent);
+
+		if(empty($requestObject->favoriteVolunteerId) === true) {
+			throw (new \InvalidArgumentException("No Volunteer linked to the Favorite", 405));
+		}
+
+		if(empty($requestObject->favoritePostId) === true) {
+			throw (new \InvalidArgumentException("No post linked to the Favorite", 405));
+		}
+
+		if(empty($requestObject->favoriteDate) === true) {
+			$requestObject->FavoriteDate = date("y-m-d H:i:s");
+		}
+
+
+		if($method === "POST") {
+
+			//enforce that the end user has a XSRF token.
+			verifyXsrf();
+
+			//enforce the end user has a JWT token
+			//validateJwtHeader();
+
+			// enforce the user is signed in
+			if(empty($_SESSION["Volunteer"]) === true) {
+				throw(new \InvalidArgumentException("you must be logged in too favorite posts", 403));
+			}
+
+			//validateJwtHeader();
+
+			$favorite= new favorite($_SESSION["volunteer"]->getVolunteerId(), $requestObject->likeTweetId);
+			$favorite->insert($pdo);
+			$reply->message = "favorite post successful";
+
+
+		} else if($method === "PUT") {
+
+			//enforce the end user has a XSRF token.
+			verifyXsrf();
+
+			//enforce the end user has a JWT token
+			//validateJwtHeader();
+
+			//grab the favorite by its composite key
+			$favorite = Favorite::getFavoriteByFavoritePostIdAndFavoriteVolunteerId($pdo, $requestObject->favoriteVolunteerId, $requestObject->likeTweetId);
+			if($favorite=== null) {
+				throw (new RuntimeException("Favorite does not exist"));
+			}
+
+			//enforce the user is signed in and only trying to edit their own like
+			if(empty($_SESSION["volunteer"]) === true || $_SESSION["volunteer"]->getVolunteerId() !== $favorite->getFavoriteVolunteerId()) {
+				throw(new \InvalidArgumentException("You are not allowed to delete this tweet", 403));
+			}
+
+			//validateJwtHeader();
+
+			//preform the actual delete
+			$favorite->delete($pdo);
+
+			//update the message
+			$reply->message = "Favorite successfully deleted";
+		}
+
+			// if any other HTTP request is sent throw an exception
+		} else {
+			throw new \InvalidArgumentException("invalid http request", 400);
+		}
+			//catch any exceptions that are thrown and update the reply status and message
+			} catch(\Exception | \TypeError $exception) {
+				$reply->status = $exception->getCode();
+				$reply->message = $exception->getMessage();
+		}
+
+		header("Content-type: application/json");
+		if($reply->data === null) {
+		unset($reply->data);
+}
+		// encode and return reply to front end caller
+		echo json_encode($reply);
+
