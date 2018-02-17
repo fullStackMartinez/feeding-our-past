de3717adfb19d80f2561bdb9e4ff2193b9734859
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-16 19:44:25 -0700

    check session, grab mysql connection, check method, sanitize input
    https://app.asana.com/0/543064250979533/560341220837895

--- public_html/api/volunteer-activation/index.php
+++ public_html/api/volunteer-activation/index.php
@@ -6,3 +6,39 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\FeedPast\Volunteer;
 
+/**
+ * API to check volunteer profile activation status
+ *
+ * @author Jolynn Pruitt <jpruitt5@cnm.edu>
+ * @author Gkephart
+ **/
+
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/feedkitty.ini");
+
+	// determine which HTTP method was used
+	// shorthand: $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if(array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER)) {
+		$method = $_SERVER["HTTP_X_HTTP_METHOD"];
+	} else {
+		$method = $_SERVER["REQUEST_METHOD"];
+	}
+
+	// sanitize input (never trust the end user)
+	$activation = filter_input(INPUT_GET, "activation", FILTER_SANITIZE_STRING);
+
+	//
+
+
+}
