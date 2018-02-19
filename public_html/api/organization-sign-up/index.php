2e440f96a38553fdf5ea3aa5f006f2d197753203
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-19 09:49:35 -0700

    wrote PUT methods for oganization sign up
    https://app.asana.com/0/543064247470613/560341220837885

--- public_html/api/organization-sign-up/index.php
+++ public_html/api/organization-sign-up/index.php
@@ -38,6 +38,62 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		//organization name is
+		//organization city address is required
+		if(empty($requestObject->organizationAddressCity) === true) {
+			throw(new \InvalidArgumentException("There is no city for organization", 405));
+		}
+		//organization state address is required
+		if(empty($requestObject->organizationAddressState) === true) {
+			throw(new \InvalidArgumentException("There is no state for organization", 405));
+		}
+		//organization street address is required
+		if(empty($requestObject->organizationAddressStreet) === true) {
+			throw(new \InvalidArgumentException("There is no state for organization", 405));
+		}
+		//organization zip code is required
+		if(empty($requestObject->organizationAddressZip) === true) {
+			throw(new \InvalidArgumentException("There is no zip code for organization", 405));
+		}
+		//the organization must indicate if donations are accepted
+		if(empty($requestObject->organizationDonationAccepted) === true) {
+			throw(new \InvalidArgumentException("Organization must indicate if donations are accepted", 405));
+		}
+		//organization email is required
+		if(empty($requestObject->organizationEmail) === true) {
+			throw(new \InvalidArgumentException("Organization email is not present", 405));
+		}
+		//organization operating hours are required
+		if(empty($requestObject->organizationHoursOpen) === true) {
+			throw(new \InvalidArgumentException("Operating hours not present", 405));
+		}
+		//organization name is required
+		if(empty($requestObject->organizationName) === true) {
+			throw(new \InvalidArgumentException("Organization name is not present", 405));
+		}
+		//organization password is required
+		if(empty($requestObject->organizationPassword) === true) {
+			throw(new \InvalidArgumentException("Must input valid password", 405));
+		}
+		//verify organization confirm password is present
+		if(empty($requestObject->organizationPasswordConfirm) === true) {
+			throw(new \InvalidArgumentException("Must input valid password", 405));
+		}
+		//organization phone number is required
+		if(empty($requestObject->organizationPhone) === true) {
+			throw(new \InvalidArgumentException("Organization phone number is not present", 405));
+		}
+		//organization url is not required, if empty set to null
+		if(empty($requestObject->organizationUrl) === true) {
+			$requestObject->organizationUrl = null;
+		}
+		//make sure the password and confirm password match
+		if($requestObject->organizationPassword !== $requestObject->organizationPasswordConfirm) {
+			throw(new \InvalidArgumentException("Passwords do not match"));
+		}
+
+		$salt = bin2hex(random_bytes(32));
+		$hash = hash_pbkdf2("sha512", $requestObject->organizationPassword, $salt, 262144);
+
+
 	}
-}
+	}
