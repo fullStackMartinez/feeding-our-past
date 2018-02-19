eb8430488f1196c0e71c16afe336eb13f242d071
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-19 14:16:10 -0700

    fixed directory path to encrypted-config.php
    https://app.asana.com/0/543064250979533/560341220837897

--- public_html/api/volunteer/index.php
+++ public_html/api/volunteer/index.php
@@ -6,7 +6,7 @@ require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 
-require_once("etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\FeedPast\Volunteer;
 
