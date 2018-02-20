a845bf80c412b336de012f53a0b2aeac30e6df87
Peter Street <PeterBStreet@gmail.com>
2018-02-20 15:47:49 -0700

    Code update after Jolynn's code review of post/index.php
    https://app.asana.com/0/543064250979533/560341220837893

--- public_html/api/post/index.php
+++ public_html/api/post/index.php
@@ -60,9 +60,10 @@ try {
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
-		//gets  a specific post associated based on its primary kdy
+		//gets  a specific post associated based on its primary key
 		if(empty($id) === false) {
 			$post = Post::getPostByPostId($pdo, $id);
+
 			if($post !== null) {
 				$reply->data = $post;
 			}
@@ -71,67 +72,68 @@ try {
 			if($post !== null) {
 				$reply->data = $post;
 			}
-		}
-	} else(empty($postEndDateTime) === false) {
-	$post = Post::getPostByPostEndDateTime($pdo, $_SESSION["organization"]->getOrganizationId())->toArray();
-	if($post !== null) {
-		$reply->data = $post;
-	}
+		} else if(empty($postEndDateTime) === false) {
+			$post = Post::getPostByPostEndDateTime($_SESSION["organization"]->getPostEndDateTime())->toArray();
+			if($post !== null) {
+				$reply->data = $post;
 
-	} else if($method === "PUT" || $method === "POST") {
-		if(empty($_SESSION["profile"]) === true) {
-			throw (new \InvalidArgumentException("You Must Be Logged In to Post", 401));
-		}
-		verifyXsrf();
-		//decode the response from the front end
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
+					} else if($method === "PUT" || $method === "POST") {
+				if(empty($_SESSION["profile"]) === true) {
 
-		if(empty($requestObject->postContent) === true) {
-			throw (new \InvalidArgumentException("No Content In Post", 405));
-		}
-		if(empty($requestObject->postId) === true) {
-			$requestObject->postEndDateTime = null;
-		}
-		if($method === "PUT") {
-			$post = Post::getPostByPostId($pdo, $id);
-			if($post === null) {
-				throw (new \RuntimeException("Post Does Not Exist", 404));
-			}
+					throw (new \InvalidArgumentException("You Must Be Logged In to Post", 401));
+				}
 
-			if(empty($_SESSION["post"]) === true || $_SESSION["post"]->getOrganizationId()
-					->toString() !== $post->getPostOrganizationId()->toString()) {
-				throw(new \InvalidArgumentException("You Are Not Allowed to Edit this Post", 403));
-			}
+				verifyXsrf();
+				//decode the response from the front end
+				$requestContent = file_get_contents("php://input");
+				$requestObject = json_decode($requestContent);
+
+				if(empty($requestObject->postContent) === true) {
+					throw (new \InvalidArgumentException("No Content In Post", 405));
+				}
+				if(empty($requestObject->postEndDateTime) === true) {
+					$requestObject->postEndDateTime = null;
+				}
+				if($method === "PUT") {
+					$post = Post::getPostByPostId($pdo, $id);
+					if($post === null) {
+						throw (new \RuntimeException("Post Does Not Exist", 404));
+					}
+
+					if(empty($_SESSION["post"]) === true || $_SESSION["post"]->getOrganizationId()
+							->toString() !== $post->getPostOrganizationId()->toString()) {
+						throw(new \InvalidArgumentException("You Are Not Allowed to Edit this Post", 403));
+					}
 //validateJwtHeader();
-			// update all attributes
-			//$post->setPostEndDateTime($requestObject->postEndDateTime);
-			$post->setPostContent($requestObject->postContent);
-			$post->update($pdo);
-			// update reply
-			$reply->message = "Post updated OK";
-		} else if($method === "POST") {
-			// enforce the user is signed in
-			if(empty($_SESSION["organization"]) === true) {
-				throw(new \InvalidArgumentException("you must be logged in to post", 403));
+					// update all attributes
+					//$post->setPostEndDateTime($requestObject->postEndDateTime);
+					$post->setPostContent($requestObject->postContent);
+					$post->update($pdo);
+					// update reply
+					$reply->message = "Post updated OK";
+				} else if($method === "POST") {
+					// enforce the user is signed in
+					if(empty($_SESSION["organization"]) === true) {
+						throw(new \InvalidArgumentException("you must be logged in to post", 403));
+					}
+					//enforce the end user has a JWT token
+					//validateJwtHeader();
+					// create new post and insert into the database
+					$post = new Post(generateUuidV4(), $_SESSION["organization"]->getOrganizationId(), $requestObject->postContent, $requestObject->EndDateTime, $requestObject->postImageUrl,  $requestObject->postStartDateTime, $requestObject->postTitle);
+					$post->insert($pdo);
+					// update reply
+					$reply->message = "Post created OK";
+				}
 			}
-			//enforce the end user has a JWT token
-			//validateJwtHeader();
-			// create new tweet and insert into the database
-			$post = new Post(generateUuidV4(), $_SESSION["organization"]->getOrganizationId(), $requestObject->postContent, null);
-			$post->insert($pdo);
-			// update reply
-			$reply->message = "Post created OK";
-		}
-	} else if($method === "DELETE") {
+		} else if($method === "DELETE") {
 			//enforce that the end user has a XSRF token.
 			verifyXsrf();
-	//retreive deleted post
-		$post = Post::getPostbyPostID($pdo,$id);
-if($post === null) {
-	throw(new RuntimeException("Post does not exisit", 404));
-}
-						if(empty($_SESSION["organization"]) === true || $_SESSION["organization"]->getOrganizationId()->toString()) {
+			//retreive deleted post
+			$post = Post::getPostbyPostID($pdo, $id);
+			if($post === null) {
+				throw(new RuntimeException("Post does not exisit", 404));
+			}
+			if(empty($_SESSION["organization"]) === true || $_SESSION["organization"]->getOrganizationId()->toString()) {
 				throw(new \InvalidArgumentException("you are not allowed to delete this post", 403));
 			}
 			//enforce the end user has a JWT token
@@ -139,15 +141,17 @@ if($post === null) {
 			//enforce the end user has a XSRF token.
 			//elete post
 			$post->delete($pdo);
-$reply->message = "Post Deleted";
-	} else {
-		throw (new InvalidArgumentException("invalid http request", 408));
+			$reply->message = "Post Deleted";
+		} else {
+			throw (new InvalidArgumentException("invalid http request", 408));
+		}
+		//catch any exceptions that is thrown and update the reply status and message
 	}
-	//catch any exceptions that is thrown and update the reply status and message
-} catch(\Exception | \TypeError $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-}
+} catch
+	(\Exception | \TypeError $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
+	}
 header("Content-type: application/json");
 if($reply->data === null) {
 	unset($reply->data);
