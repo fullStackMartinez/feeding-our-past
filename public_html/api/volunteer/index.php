64a88a72c30a81758467269609c38dabe211530e
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-21 14:23:41 -0700

    fixed path to feedkitty.ini file (missing leading /)
    https://app.asana.com/0/543064250979533/560341220837897

--- public_html/api/volunteer/index.php
+++ public_html/api/volunteer/index.php
@@ -28,7 +28,7 @@ $reply->data = null;
 
 try {
 	// grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("etc/apache2/capstone-mysql/feedkitty.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/feedkitty.ini");
 
 	// determine HTTP method used
 	// shorthand: $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
