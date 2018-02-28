46c85cdac67c411fe315ac19674e370fe21c73ed
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-28 14:18:13 -0700

    made sign in forms in organization-sign-in and volunteer-sign-in. Still need href's for both

--- static-ui/organization-sign-in.php
+++ static-ui/organization-sign-in.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Esteban
- * Date: 2/28/2018
- * Time: 2:04 PM
- */
+
+
+<form>
+  <div class="form-group">
+    <label for="inputEmail">Email address</label>
+    <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter organization email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword">Password</label>
+    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+  </div>
+	<button type="submit" class="btn btn-primary">Log in</button>
+</form>
+<h4><em>Not yet registered?</em>| <a href=""><strong>Sign Up</strong></a> </h4>
+
---
+++ static-ui/volunteer-sign-in.php
@@ -0,0 +1,13 @@
+<form>
+	<div class="form-group">
+		<label for="inputEmail">Email address</label>
+		<input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter volunteer email">
+		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	</div>
+	<div class="form-group">
+		<label for="inputPassword">Password</label>
+		<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+	</div>
+	<button type="submit" class="btn btn-primary">Log in</button>
+</form>
+<h4><em>Not yet registered?</em>| <a href=""><strong>Sign Up</strong></a> </h4>
