ae5fe1282760d13ac062cd97406cd6acf9e2b0ee
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-28 15:42:34 -0700

    added organization url field and indicated required fields

--- static-ui/organization-sign-up.php
+++ static-ui/organization-sign-up.php
@@ -9,55 +9,58 @@
 				<h2>Organization Sign-Up</h2>
 			</div>
 			<form>
+				<small id="formRequired" class="form-text text-danger">*Required fields</small>
 				<div class="form-group">
-					<label for="organizationName">Organization Name</label>
+					<label for="organizationName">Organization Name <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationName" placeholder="Organization name">
 				</div>
 				<div class="form-group">
-					<label for="organizationEmail">Email address</label>
+					<label for="organizationEmail">Email address <span class="text-danger">*</span></label>
 					<input type="email" class="form-control" id="organizationEmail" aria-describedby="emailHelp" placeholder="Enter email">
 					<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 				</div>
 				<div class="form-group">
-					<label for="organizationPassword">Password</label>
+					<label for="organizationPassword">Password <span class="text-danger">*</span></label>
 					<input type="password" class="form-control" id="organizationPassword" placeholder="Password">
 				</div>
 				<div class="form-group">
-					<label for="organizationPasswordConfirm">Confirm password</label>
+					<label for="organizationPasswordConfirm">Confirm password <span class="text-danger">*</span></label>
 					<input type="password" class="form-control" id="organizationPasswordConfirm" placeholder="Confirm password">
 				</div>
 				<div class="form-group">
-					<label for="organizationAddressStreet">Street Address</label>
+					<label for="organizationAddressStreet">Street Address <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationAddressStreet" placeholder="Street address">
 				</div>
 				<div class="form-group">
-					<label for="organizationAddressCity">City</label>
+					<label for="organizationAddressCity">City <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationAddressCity" placeholder="City">
 				</div>
 				<div class="form-group">
-					<label for="organizationAddressState">State</label>
+					<label for="organizationAddressState">State <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationAddressState" placeholder="State">
 				</div>
 				<div class="form-group">
-					<label for="organizationAddressZip">Zip Code</label>
+					<label for="organizationAddressZip">Zip Code <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationAddressZip" placeholder="Zip code">
 				</div>
 				<div class="form-group">
-					<label for="organizationPhone">Phone number</label>
+					<label for="organizationPhone">Phone number <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationPhone" placeholder="Phone number (xxx-xxx-xxxx)">
 				</div>
 				<div class="form-group">
-					<label for="organizationHoursOpen">Days/Hours Open</label>
+					<label for="organizationHoursOpen">Days/Hours Open <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationHoursOpen" placeholder="Hours open">
 				</div>
 				<div class="form-group">
-					<label for="organizationDonationsAccepted">Food donations accepted?</label>
+					<label for="organizationDonationsAccepted">Food donations accepted? <span class="text-danger">*</span></label>
 					<input type="text" class="form-control" id="organizationDonationsAccepted" placeholder="(yes/no)">
+				</div>
+				<div class="form-group">
+					<label for="organizationUrl">Website</label>
+					<input type="text" class="form-control" id="organizationUrl" placeholder="Website address">
 				</div>
 				<button type="submit" class="btn btn-primary">Submit</button>
 			</form>
-
 		</div>
-
 	</body>
 
