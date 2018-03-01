487c838dcd1edfec56bb20a9da8f4a20f49ef4d2
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-01 16:51:24 -0700

    added home-view

---
+++ static-ui/home-view.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once ("head-utils.php");?>
+
+	<body>
+		<div class="container bg-secondary">
+			<div class="row">
+				testing testing
+			</div>
+		</div>
+
+		<div class="container p-4 bg-info">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card mb-4">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container mt-5">
+			<div class="row justify-content-center">
+				<div class="col-md-6">
+					<div class="card mb-4">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</body>
+</html>
