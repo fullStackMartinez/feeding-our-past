2685595ad981d9fb019d50ead692a49b1324f967
Peter Street <PeterBStreet@gmail.com>
2018-03-01 16:38:09 -0700

    Static-ui/headder c/p

--- static-ui/headder.php
+++ static-ui/headder.php
@@ -10,7 +10,6 @@
 
 		<!--Here I created a div and created a container. my is margins top and bottom mx is margins left to right -->
 		<div class="container my-3 mx-5">
-			<!-- navbar with "Bootstrap Challenge" Title and 4 "links" aligned right.
 			 Here I created a navbar that will expanded when medium. with light text and background and rounded border-->
 			<nav class="navbar navbar-expand-md navbar-info justify-content-center bg-light border rounded">
 				<!-- the  navbar-brand gets our text in the nav bar -->
