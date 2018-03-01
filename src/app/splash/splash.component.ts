edc02ec85d69016bfa39c5ad4e57ec4a861d163e
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-01 11:31:40 -0700

    created shared directory in app directory created classes directory and services directory in shared created files in those directories

---
+++ src/app/splash/splash.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from "@angular/core";
+import {User} from "../shared/classes/user";
+import {UserService} from "../shared/services/user.service";
+
+@Component({
+	template: require("./splash.component.html")
+})
+
+export class SplashComponent implements OnInit {
+	users: User[] = [];
+
+	constructor(private userService: UserService) {}
+
+	ngOnInit(): void {
+		this.userService.getAllUsers()
+			.subscribe(users => this.users = users);
+	}
+}
