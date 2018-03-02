3b46c0a218c5e85b8b3f895803d456edfb0428c4
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-02 13:26:05 -0700

    linked buttons to static ui pages

--- static-ui/home-view.php
+++ static-ui/home-view.php
@@ -15,8 +15,8 @@
 						<div class="card-body bg-white">
 							<h1 class="card-title">Organization</h1>
 							<p class="card-text text-muted">this is aligned left text.  the text will hopefully enlarge the size of the card.</p>
-							<button type="button" class="btn btn-primary">Sign up</button>
-							<button type="button" class="btn btn-primary">Sign in</button>
+							<input type="button" onclick="location.href='organization-sign-up.php';" value="Sign up" />
+							<input type="button" onclick="location.href='organization-sign-in.php';" value="Sign in" />
 						</div>
 					</div>
 				</div>
@@ -25,7 +25,7 @@
 						<div class="card-body bg-white text-center">
 							<h1 class="card-title">Seniors</h1>
 							<p class="card-text">If you are in need of food assistance please click here.</p>
-							<button type="button" class="btn btn-primary">I need help</button>
+							<input type="button" onclick="location.href='organization-sign-up.php';" value="Sign up" />
 						</div>
 					</div>
 				</div>
@@ -34,8 +34,8 @@
 						<div class="card-body bg-white text-right">
 							<h1 class="card-title">Volunteers</h1>
 							<p class="card-text text-muted">this is aligned left text.  the text will hopefully enlarge the size of the card.</p>
-							<button type="button" class="btn btn-primary">I can help</button>
-							<button type="button" class="btn btn-primary">sign in</button>
+							<input type="button" onclick="location.href='volunteer-sign-up.php';" value="sign up" />
+							<input type="button" onclick="location.href='volunteer-sign-in.php';" value="Sign in" />
 						</div>
 					</div>
 				</div>
