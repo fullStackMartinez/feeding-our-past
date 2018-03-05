9b07af6a9461405a55885cb9b31214fbaa3a73cd
Peter Street <PeterBStreet@gmail.com>
2018-03-05 11:16:17 -0700

    Post API re-fix

--- static-ui/headder.php
+++ static-ui/headder.php
@@ -1,26 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-	<?php require_once ("head-utils.php");?>
+	<?php require_once("head-utils.php"); ?>
 	<body>
-		<!--<nav class="navbar float-top navbar-dark bg-info justify-content-center ">
-			<a class="navbar-brand" href="#">Feeding Our Past</a>
-		</nav>-->
-
-
-
-		<!--Here I created a div and created a container. my is margins top and bottom mx is margins left to right -->
-		<div class="container">
-			<nav class="navbar navbar-expand-md navbar-info navbar-light bg-light border rounded fixed-top">
-				<!-- the  navbar-brand gets our text in the nav bar -->
+		<div >
+			<nav class="navbar float-top navbar-dark bg-info justify-content-center fixed-top">
 				<a class="navbar-brand" href="#">Feeding Our Past</a>
-				<!--Here I created buttons that collapse in white. Again sample  -->
+			</nav>
+		</div>
+		<div class="container">
+			<nav class="navbar float navbar-expand-md navbar-info justify-content-center navbar-light bg-light border rounded">
+				<a class="navbar-brand" href="#">Feeding Our Past</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-				<!-- Here I continue the with the collapsable button "elements" and order them from left to right and justify them on the right or end -->
 				<div class="collapse navbar-collapse justify-content-center" id="navbarToggler">
-					<!-- Here I create the unordered list which creates the "links"
-					The mt settings affect the thickness of the navbar-->
 					<ul class="navbar-nav ml-auto mt-0 mt-sm-0">
 						<li class="nav-item">
 							<a class="nav-link" href="#">We Help</a>
@@ -28,7 +21,6 @@
 						<li class="nav-item align-items-center">
 							<a class="nav-link" href="#">I Need Help</a>
 						</li>
-
 						<li class="nav-item">
 							<a class="nav-link" href="#">I Want to Help</a>
 						</li>
@@ -38,6 +30,5 @@
 					</ul>
 				</div>
 			</nav>
-	</div>
 	</body>
 </html>
