b47a11fb9b325d247ef352cb6b422706fc9ac9f1
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-06 09:58:51 -0700

    created volunteer sign in service

---
+++ src/app/shared/services/volunteer.sign.in.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {Status} from "../classes/status";
+import {VolunteerSignIn} from "../classes/volunteer.sign.in";
+
+@Injectable()
+export class VolunteerSignInService {
+	constructor(protected http : HttpClient) {
+
+	}
+
+	private volunteerSignInUrl = "api/volunteer-sign-in/";
+	private volunteerSignOutUrl = "api/sign-out";
+
+
+
+	//preform the post to initiate sign in
+	postVolunteerSignIn(volunteerSignIn:VolunteerSignIn) : Observable<Status> {
+		return(this.http.post<Status>(this.volunteerSignInUrl, volunteerSignIn));
+	}
+
+	volunteerSignOut() : Observable<Status> {
+		return(this.http.get<Status>(this.volunteerSignOutUrl));
+	}
+
+}
