b43ae96ca44520360e1109126d5c3e9e307942da
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-07 09:01:26 -0700

    added imports to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import {RouterModule, Routes} from "@angular/router";
+import {AuthGuardService as AuthGuard} from "./shared/guards/auth.guard";
+import {AuthService} from "./shared/services/auth.service";
+import {FavoriteService} from "./shared/services/favorite.service";
 import {HomeComponent} from "./home/home.component";
 import {OrganizationService} from "./shared/services/organization.service";
+import {OrganizationSignInService} from "./shared/services/organization.sign.in.service";
+import {OrganizationSignUpService} from "./shared/services/organization.sign.up.service";
+import {PostService} from "./shared/services/post.service";
+import {SessionService} from "./shared/services/session.service";
+import {VolunteerService} from "./shared/services/volunteer.service";
+import {VolunteerSignInService} from "./shared/services/volunteer.sign.in.service";
+import {VolunteerSignUpService} from "./shared/services/volunteer.sign.up.service";
 
 
 export const allAppComponents = [HomeComponent];
