56d96b50b38fffda427e80dc1c4ee497eb1f6876
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-07 08:51:23 -0700

    edited home.component to get posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Post} from "../shared/classes/post";
+import {Status} from "../shared/classes/status";
+import {PostService} from "../shared/services/post.service";
 
 @Component({
 	template: require("./home.html")
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+	posts: Post[] = [];
+	status: Status = null;
 
+	constructor(
+		private postService: PostService
+	){}
+
+	ngOnInit() : void {
+		this.listPosts();
+	}
+
+	listPosts(): void {
+		this.postService.getPostByPostEndDateTime()
+			.subscribe(posts => this.posts = posts);
+	}
 }
+
