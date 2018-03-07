dc27513299454dc32e55717616b87cb50ba4c6a2
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-07 15:54:38 -0700

    added static ui for senior view

---
+++ static-ui/senior-view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<?php require_once("head-utils.php"); ?>
+
+	<body>
+		<?php require_once("header.php"); ?>
+		<section>
+			<div class="container mt-5 pt-5">
+				<div class="row justify-content-center">
+					<div class="col-md-8">
+						MAP
+					</div>
+
+					<!-- right column for list of organizations -->
+					<div class="col-md-4">
+						<div class="card mb-4">
+							<div class="card-body text-center">
+								<h2 class="card-title">Organization List</h2>
+								<p class="card-text">This card will have list of organizations.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
