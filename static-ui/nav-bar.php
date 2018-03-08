c1903401ce1a3108e67d3cff87e1be2c1872c48a
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-08 11:09:13 -0700

    finished main nav component.ts

--- static-ui/nav-bar.php
+++ static-ui/nav-bar.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
+
 <html lang="en">
+		<?php require_once ("head-utils.php");?>
 
-		<?php require_once("head-utils.php"); ?>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+		<a class="navbar-brand" href="#">Feeding Our Past</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-			<nav class="navbar navbar-expand-lg navbar-dark">
-				<a class="navbar-brand" routerLink="/"> Feeding Our Past </a><small class="d-none d-md inline-block text-muted mr-auto"><em> A Capstone Project </em></small>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				</button>
+				<a class="nav-link dropdown-toggle-left" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Dropdown
+				</a>
 
-				<div *ngIf="isAuthenticated === true" class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav ml auto">
-						"<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Hello, <span class="text-warning">{{ profileUsername }}</span>
-							</a>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle-left" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Dropdown
+					</a>
 
+					<form class="form-inline my-2 my-lg-0">
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Make a New Post</a>
+						<a class="dropdown-item" href="#">Sign Out</a>
+						<div class="dropdown-divider"></div>
 
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" routerLink="/posts"><i class="fa fa-pencil"></i>&nbsp;&nbsp;New Post</a>
-								<div class="dropdown-divider"></div>
-								<button class="btn-link dropdown-item" (click)="signOut();"><i class="fa fa-sign-out"></i>&nbsp;&nbsp;Sign Out</button>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-
-
-
-	<body>
-
-	</body>
-</html>
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		</div>
+	</nav>
