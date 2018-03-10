82809089474b202409f781773964d3982519fc68
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-09 20:01:45 -0700

    - moved about static-ui code to the component.html - added code for post form

--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 //import {JwtHelperService} from "@auth0/angular-jwt";
 import {Status} from "../shared/classes/status";
 import {Organization} from "../shared/classes/organization";
@@ -6,7 +7,7 @@ import {OrganizationService} from "../shared/services/organization.service";
 import {PostService} from "../shared/services/post.service";
 import {Post} from "../shared/classes/post";
 import {PostAuthor} from "../shared/classes/post.author";
-
+import {AuthService} from "../shared/services/auth.service";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
 import "rxjs/add/observable/from";
@@ -17,24 +18,70 @@ import "rxjs/add/operator/switchMap";
 })
 
 export class OrganizationComponent implements OnInit {
+
+	createPostForm: FormGroup;
 	posts: PostAuthor[] = [];
+	newpost: Post[] = []
 	organization: Organization = new Organization(null, null, null, null, null, null, null, null, null, null, null, null, null, null );
 	status: Status = null;
 
-	constructor(private organizationService: OrganizationService,
-					//		private jwtHelper : JwtHelperService,
-					private postService: PostService) {
+	constructor(
+		private formBuilder: FormBuilder,
+		private authService: AuthService,
+		private organizationService: OrganizationService,
+		//private jwtHelper : JwtHelperService,
+		private postService: PostService) {
 	}
 
 	ngOnInit(): void {
 		this.listPosts();
 //		this.currentlySignedIn()
+
+		this.createPostForm = this.formBuilder.group({
+			postTitle: ["", [Validators.maxLength(255), Validators.required]],
+			postStartDateTime: ["", [Validators.maxLength(32), Validators.required]],
+			postEndDateTime: ["", [Validators.maxLength(32), Validators.required]],
+			postContent: ["", [Validators.maxLength(4096), Validators.required]]
+		})
 	}
 
 	listPosts(): void {
 		this.postService.getPostByPostEndDateTime()
 			.subscribe(posts => {
 				this.posts = posts;
+			});
+	}
+
+/*	getJwtProfileId() : any {
+		if(this.authService.decodeJwt()) {
+			return this.authService.decodeJwt().auth.organizationId;
+		} else {
+			return false
+		}
+	}*/
+
+	createPost(): void {
+
+/*		// if no JWT organizationId, return false (must be logged in to post)
+		if(!this.getJwtProfileId()) {
+			return false
+		}
+
+		// grab organizationId off of JWT
+		let newPostOrganizationId = this.getJwtProfileId(); */
+
+		// create new post
+		let newPost = new Post(null, null, this.createPostForm.value.postContent, this.createPostForm.value.postEndDateTime, null, this.createPostForm.value.postStartDateTime, this.createPostForm.value.postTitle);
+
+		this.postService.createPost(newPost)
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.listPosts();
+					this.createPostForm.reset();
+				}else{
+					console.log('no valid login');
+				}
 			});
 	}
 
