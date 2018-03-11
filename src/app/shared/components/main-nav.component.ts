3bd0f72a0c001edccfb58b5f504154e22b074978
Jolynn Pruitt <jo@tekiosolutions.com>
2018-03-10 18:44:50 -0700

    added import sign in services

--- src/app/shared/components/main-nav.component.ts
+++ src/app/shared/components/main-nav.component.ts
@@ -3,43 +3,48 @@ import {Status} from "../classes/status";
 import {AuthService} from "../services/auth.service";
 import {CookieService} from "ng2-cookies";
 import {Router} from "@angular/router";
-
+import {VolunteerSignInService} from "../services/volunteer.sign.in.service";
+import {OrganizationSignInService} from "../services/organization.sign.in.service";
 
 
 @Component({template: require("./main-nav.component.html"),
 				selector: "navbar"
 })
 
-export class NavbarComponent implements OnInit {
+export class NavbarComponent /*implements OnInit*/ {
 			status: Status = null;
 			isAuthenticated: any = null;
-			organizationUsername: string = null;
-			organizationId: string = null;
+			//organizationUsername: string = null;
+			//organizationId: string = null;
 
 			constructor(
-				//private signInService: SignInService,
+				private organizationSignInService: OrganizationSignInService,
+				private volunteerSignInService: VolunteerSignInService,
 				private cookieService: CookieService,
 				private authService: AuthService,
 				private router: Router
 			){}
 
-			ngOnInit(): void {
+			/*ngOnInit(): void {
 				this.isAuthenticated = this.authService.loggedIn();
 				this.organizationId  = this.getOrganizationId();
 				//this.organizationName = this.getOrganizationName();
-			}
+			}*/
 
 
 	signOut() : void {
-		this.signInService.getSignOut()
+		this.volunteerSignInService.SignOut()
+		this.organizationSignInService.SignOut()
 
 			.subscribe(status => {
 				this.status = status;
 				if(status.status === 200) {
 
+					// delete cookies and jwt
 					this.cookieService.deleteAll();
 					localStorage.clear();
 
+					// send user back home, refresh page
 					this.router.navigate([""]);
 					location.reload();
 					console.log("goodbye");
